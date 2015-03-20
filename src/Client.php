fafa619029dfbd5fa5d74875671d8977afd4b3b4
Kyle <student@epicodus.com>
2015-03-20 12:06:17 -0700

    Add update method to Client

--- src/Client.php
+++ src/Client.php
@@ -89,8 +89,11 @@
             $GLOBALS['DB']->exec("DELETE FROM clients WHERE id = {$this->getId()};");
         }
 
-
-        //function update()
+        function update($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE clients SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
 
         static function deleteAll()
         {
