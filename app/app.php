3afc37b61fa93ecbdb6a5c109aafa8c3c83f767f
Kyle <student@epicodus.com>
2015-03-20 12:27:36 -0700

    Add app.php skeleton

---
+++ app/app.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+    $app = new Silex\Application();
+
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig');
+    });
+
+?>
