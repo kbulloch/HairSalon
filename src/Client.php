919c80f4dfc3a8e35d4ed8a062e70017dcffb2bc
Kyle <student@epicodus.com>
2015-03-20 10:56:48 -0700

    Add Client.php skeleton

---
+++ src/Client.php
@@ -0,0 +1,24 @@
+<?php
+
+    /***********
+    *Your class should have the following methods:
+    __construct, getters and setters for all properties,
+    save, getAll, deleteAll, find, update, and delete.
+    *************/
+
+
+    class Client
+    {
+        private $name;
+        private $stylist_id;
+        private $id;
+
+        function __construct($name, $stylist_id, $id = null)
+        {
+            $this->name = $name;
+            $this->stylist_id = $stylist_id;
+            $this->id = $id;
+        }
+    }
+
+?>
