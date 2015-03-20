e35b387c92c57393c9dc1a3c683661a2c3d37c79
Kyle <student@epicodus.com>
2015-03-20 11:01:51 -0700

    Add test_getName to ClientTest

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -15,5 +15,20 @@
         {
             Client::deleteAll();
         }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Francis";
+            $stylist_id = 111;
+            $id = null;
+            $test_client = new Client($name, $stylist_id, $id);
+
+            //Act
+            $result = $test_client->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
     }
 ?>
